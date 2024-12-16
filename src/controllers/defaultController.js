39b4009bbbadae7a68ceef33e036c54cec586ed9
Suah-Cho <sacho0929@gmail.com>
2024-12-16 17:44:13 +0900

    feat: #1 register user

---
+++ src/controllers/defaultController.js
@@ -0,0 +1,13 @@
+const user = require('../../models/users');
+const connection = require('../../config/db_utils');
+
+const login = async (req, res) => {
+    // email & password
+    const { username, password } = req.body;
+
+    // try {
+
+    // }
+};
+
+module.exports = { login };
--- src/routes/defaultRoutes.js
+++ src/routes/defaultRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { login } = require('../controllers/defaultController');
 
 const router = express.Router();
 
-// 기본 라우트
 router.get('/', (req, res) => {
     res.status(200).send('Express server API is running...');
 });
 
-module.exports = router; // 라우터 내보내기
+router.post('/login', login);
+
+module.exports = router;
